a88fc9d3736d13c090ba5bc1a938376e393fb6bf
Imanpreet singh <125171793+gavi04@users.noreply.github.com>
2024-11-24 00:06:58 +0530

    health check index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const JWT_SECRET = 'helloworld';
 
 app.use(express.json());
 app.use(cors());
+//heath check
+app.get('/health', (req, res) => {
+  res.status(200).send({ status: 'ok' });
+});
 // Create a User
 app.post('/signup', async (req, res) => {
   const { email, password,  name } = req.body;
